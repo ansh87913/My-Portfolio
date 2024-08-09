e5f7999a4d27bf335670ab3418b1200a3485fa09
ansh87913 <96718331+ansh87913@users.noreply.github.com>
2024-08-09 08:38:08 +0530

    Update projectsData.js

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -4,6 +4,14 @@ import P3 from "../../images/Project3.png";
 
 const projectsData = [
   {
+    id: 1,
+    imageUrl: P3,
+    tags: ["JavaScript", "React", "HTML", "CSS", "Bootstrap"],
+    title: "Portfolio",
+    description: "Created a responsive personal portfolio website using React.js, JavaScript, HTML, CSS, and Bootstrap. It includes a dynamic UI all styled with Bootstrap and CSS for a modern design and optimized for all devices",
+    github: "https://github.com/ansh87913/My-Portfolio",
+  },
+  {
     id: 2,
     imageUrl: P1,
     tags: ["Express JS", "MongoDB", "Node JS", "JavaScript", "React"],
@@ -19,14 +27,6 @@ const projectsData = [
     description: "This Calculator is a user-friendly application built using React.js, designed to perform basic arithmetic operations such as addition, subtraction, multiplication, and division. It features an intuitive interface with real-time calculation display, keyboard support",
     github: "https://github.com/ansh87913/React-Frontend-2",
   },
-  {
-    id: 1,
-    imageUrl: P3,
-    tags: ["JavaScript", "React", "HTML", "CSS", "Bootstrap"],
-    title: "Portfolio",
-    description: "Created a responsive personal portfolio website using React.js, JavaScript, HTML, CSS, and Bootstrap. It includes a dynamic UI all styled with Bootstrap and CSS for a modern design and optimized for all devices",
-    github: "https://github.com/ansh87913/My-Portfolio",
-  }
 ];
 
 export default projectsData;
